c945be32e7c1fd79d65ea83436162a8bad98df87
Christin Morton <contactme@christinmorton.com>
2024-11-30 23:13:44 -0800

    bug: attempt to fix token error for frontend authentication.

--- backend/config/middlewares.ts
+++ backend/config/middlewares.ts
@@ -5,16 +5,27 @@ export default [
   {
     name: 'strapi::cors',
     config: {
-      origin: ['https://cmtestwebsite7.com'],
+      origin: ['https://cmtestwebsite7.com', 'http://localhost:3000'],
       methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
-      headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
+      headers: ['Content-Type', 'Authorization', 'Origin', 'Accept', 'Token'],
       keepHeaderOnError: true,
     },
   },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
-  'strapi::session',
+  {
+    name: 'strapi::session',
+    config: {
+      key: 'ascsn-token', // Default cookie key
+      maxAge: 86400000, // 1 day in milliseconds
+      autoCommit: true, // Automatically commit headers
+      overwrite: true, // Allow overwriting of cookies
+      signed: true, // Sign the cookies
+      renew: true, // Renew session if nearing expiration
+      secure: process.env.NODE_ENV === 'production', // Use HTTPS in production
+    },
+  },
   'strapi::favicon',
   'strapi::public',
 ];
